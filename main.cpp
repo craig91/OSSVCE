a5e4b4d6e7ea15b6739152bd11f2fc5e21179c88
craig91 <craigdejean1@gmail.com>
2025-06-19 23:45:04 +0900

    library test

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include "math_headers.h"
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int a = 10, b = 5;
+
+    cout << "Addition: " << add(a, b) << endl;
+    cout << "Subtraction: " << subtract(a, b) << endl;
+
+    return 0;
+
+}
